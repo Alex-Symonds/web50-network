46fb0019e090ca28a7adb195ab8086daa46cee9a
Alex Symonds <alex-symonds@gmail.com>
2021-06-18 11:35:56 +0100

    Models updated for User and Post

--- network/models.py
+++ network/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField("self", related_name="followers")
+
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_on = models.DateTimeField(auto_now_add=True)
+    contents = models.CharField(max_length=280)
+    likes = models.ManyToManyField(User, related_name="liked")
+
