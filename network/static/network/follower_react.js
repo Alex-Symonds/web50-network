5e7b6b62c433e94c9e8ae06e4c62bae613ba3d55
Alex Symonds <alex-symonds@gmail.com>
2021-06-28 15:36:59 +0100

    Follow counter tidied

--- network/static/network/follower_react.js
+++ network/static/network/follower_react.js
@@ -1,22 +1,23 @@
-function Followers(props){
+function Followers(){
     // Constants that might need altering
     const DISPLAY_TRUE = 'following';
     const DISPLAY_FALSE = 'not following';
-    const CLASS_BOTH = 'follow-btn ';
+    
+    const CLASS_BOTH = 'follow-btn';
     const CLASS_TRUE = 'following';
     const CLASS_FALSE = 'notfollowing';
 
     // Initialise with the help of global values from Django
     const [followers, setFollowers] = React.useState({
         num_followers: num_followers,
-        display_str: get_display_str(is_following),
-        class_list: get_class_list(is_following),
-        following: is_following
+        following: str_following === 'true',
+        display_str: get_display_str(str_following === 'true'),
+        class_list: get_class_list(str_following === 'true')
     });
 
     // Select the display string to appear inside the button
     function get_display_str(isf){
-        if ('true' === isf){
+        if (isf){
             return DISPLAY_TRUE;
         } else {
             return DISPLAY_FALSE;
@@ -25,14 +26,14 @@ function Followers(props){
 
     // Select the CSS class list for the button
     function get_class_list(isf){
-        if ('true' === isf){
-            return CLASS_BOTH + CLASS_TRUE;
+        if (isf){
+            return CLASS_BOTH + ' ' + CLASS_TRUE;
         } else {
-            return CLASS_BOTH + CLASS_FALSE;
+            return CLASS_BOTH + ' ' + CLASS_FALSE;
         }
     }
 
-    // Toggle following status
+    // Toggle "following" status
     function toggleFollowers(e){
         // Update follow status on the backend via the toggle function
         toggle(e);
@@ -40,9 +41,9 @@ function Followers(props){
         // Update follow status on the front end
         // Set the new status and increment/decrement number of followers
         var updated_followers = followers.num_followers;
-        var toggled = 'true';
-        if ('true' === followers.following){
-            toggled = 'false';
+        var toggled = true;
+        if (followers.following){
+            toggled = false;
             updated_followers--;
         } else {
             updated_followers++;
@@ -51,22 +52,20 @@ function Followers(props){
         // Update the state accordingly
         setFollowers({
             num_followers: updated_followers,
+            following: toggled,
             display_str: get_display_str(toggled),
-            class_list: get_class_list(toggled),
-            following: toggled
+            class_list: get_class_list(toggled)
         });
     }
 
-    // Display on the page
+    //If the user is logged in and it's not their own profile, display the counter and the toggle button
     if('True' === user_isauth && (username != profile)){
-        return (
-            <div>
-                <FollowerCounter count={followers.num_followers} />
+        return [
+                <FollowerCounter count={followers.num_followers} />,
                 <button id="follower_toggle_react" class={followers.class_list} data-id={user_id} data-status={followers.following} data-viewname="follow" onClick={(e) => {toggleFollowers(e)}}>{followers.display_str}</button>
-            </div>
-        );
+        ];
     }
-
+    // Otherwise, just display the counter
     return <FollowerCounter count={followers.num_followers} />;
 }
 
